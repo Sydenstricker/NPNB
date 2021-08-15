80adfbba872598d31f87c21226937fcae5cabffe
Fernando <sydenstricker.sony@gmail.com>
2021-08-15 18:32:34 -0300

    Bug drop PI, tela morte
    concertou o drop dos PI, tela de morte concertada.

--- PB COM CUIDADO/Assets/Scripts/Enemy.cs
+++ PB COM CUIDADO/Assets/Scripts/Enemy.cs
@@ -96,10 +96,10 @@ public class Enemy : MonoBehaviour
     private void SpawnRandomPI()
     {
         inimigosSoltamPI = Random.Range(chanceMinSoltarPID,chanceMaxSoltarPID);
-        if (inimigosSoltamPI == 10)
+        if (inimigosSoltamPI >= 10)
         {
             GameObject PI = Instantiate(PIPrefab, transform.position, Quaternion.identity) as GameObject;
         }
-        else return;
+        //else return;
     }
 }
