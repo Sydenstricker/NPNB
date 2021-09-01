3a264e0912261a2a940716f9b477b236f54ae701
Fernando <sydenstricker.sony@gmail.com>
2021-09-01 16:04:55 -0300

    Morcego Cosmetico prefab. Moeda Ronie implementada.

--- PB COM CUIDADO/Assets/Scripts/Cave/EnemyCav.cs
+++ PB COM CUIDADO/Assets/Scripts/Cave/EnemyCav.cs
@@ -4,25 +4,50 @@ using UnityEngine;
 
 public class EnemyCav : MonoBehaviour
 {
-            
+
+    private Animator animator;
+    [SerializeField] private float morcegoPersegue = 1f;
     //[SerializeField] GameObject spriteTomouDano; spawna sprite quando toma dano
        
     void Start()
     {
+        animator = GetComponent<Animator>();
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        
     }
         
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.gameObject.layer == 8)
+        {
+            animator.SetTrigger("VoaMorcego");
+            //transform.position = new Vector3(-0.01f,-0.05f,0);
+            //PerseguePlayer();
+
+        }
+
         DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
         if (!damageDealer) { return; }        
     }
-      
+
+    private void MovimentaMorcegoDiagonal()
+    {
+        transform.position = new Vector2(-0.1f, -0.1f);
+        Debug.Log("Morcego persegue Player loucamente");
         
+    }
+
+    private void PerseguePlayer()
+    {
+        transform.position = FindObjectOfType<PlayerCav>().gameObject.transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, transform.position, Time.deltaTime);
+    }
+
 }
