3b1f7070bd3602b5c73e7a473dcf5e8a86a0a671
Fernando <sydenstricker.sony@gmail.com>
2021-09-08 16:34:57 -0300

    Animação lobo Flipando correta

--- PB COM CUIDADO/Assets/Scripts/Cave/EnemyCav.cs
+++ PB COM CUIDADO/Assets/Scripts/Cave/EnemyCav.cs
@@ -6,10 +6,12 @@ public class EnemyCav : MonoBehaviour
 {
 
     private Animator animator;
+    private Rigidbody2D rigidBody2D;
     //[SerializeField] GameObject spriteTomouDano; spawna sprite quando toma dano
        
     void Start()
     {
+        rigidBody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();  
     }
                 
@@ -21,10 +23,34 @@ public class EnemyCav : MonoBehaviour
             //transform.position = new Vector3(-0.01f,-0.05f,0);
             //PerseguePlayer();
         }
+        if (other.gameObject.layer == 15)
+        {
+            transform.rotation = Quaternion.AngleAxis(180, Vector3.down);
+            //Quaternion AngleAxis(float angle, Vector3 axis);
+            //transform.rotation = Quaternion.Euler(0, -180f, 0);
+            Debug.Log("flipa lobo wuf wuf");
+        }
+        if (other.gameObject.layer == 16)
+        {
+            transform.rotation = Quaternion.AngleAxis(180, Vector3.zero);
+            //Quaternion AngleAxis(float angle, Vector3 axis);
+            //transform.rotation = Quaternion.Euler(0, -180f, 0);
+            Debug.Log("desflipa lobo wuf wuf");
+        }
         DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
         if (!damageDealer) { return; }        
     }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 15)
+        {
+            //transform.rotation = Quaternion.AngleAxis(180, Vector3.zero);
+            //Quaternion AngleAxis(float angle, Vector3 axis);
+            //transform.rotation = Quaternion.Euler(0, -180f, 0);
+            Debug.Log("gira lobo wuf wuf");
+        }
+    }
     private void MovimentaMorcegoDiagonal()
     {
         transform.position = new Vector2(-0.1f, -0.1f);
