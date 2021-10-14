bb2dac3d77a7424c737522b7502532aa8152be00
Jéssica <jeunsantos@gmail.com>
2021-10-14 14:06:15 -0300

    Animação Waffle e Arma Blu

--- PB COM CUIDADO/Assets/Scripts/Cave/MoedaCav.cs
+++ PB COM CUIDADO/Assets/Scripts/Cave/MoedaCav.cs
@@ -6,7 +6,7 @@ public class MoedaCav : MonoBehaviour
 {
     [SerializeField] AudioClip coletaSFX;
     [SerializeField] [Range(0, 1)] float volumeColeta = 0.75f;
-    [SerializeField] int scoreValue = 150;
+    [SerializeField] int scoreValue = 10;
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D other)
     {
