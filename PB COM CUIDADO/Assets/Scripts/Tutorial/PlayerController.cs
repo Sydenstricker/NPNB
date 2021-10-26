a6d2310c74063fabc3ae0f19953ab3e43c068960
Fernando <sydenstricker.sony@gmail.com>
2021-10-26 10:53:24 -0300

    SFX Tutorial ok

--- PB COM CUIDADO/Assets/Scripts/Tutorial/PlayerController.cs
+++ PB COM CUIDADO/Assets/Scripts/Tutorial/PlayerController.cs
@@ -21,7 +21,30 @@ public class PlayerController : MonoBehaviour
 
     // Pulo duplo
     [SerializeField] private bool puloDouble = false;
-    [SerializeField] private int puloCount = 0;    
+    [SerializeField] private int puloCount = 0;
+
+    [Header("Player Audio")]
+    [SerializeField] AudioClip deathSFX;
+    [SerializeField] [Range(0, 1)] float volumeMorte = 0.75f;
+    [SerializeField] AudioClip pulo1SFX;
+    [SerializeField] [Range(0, 1)] float volumePulo1 = 0.75f;
+    [SerializeField] AudioClip pulo2SFX;
+    [SerializeField] [Range(0, 1)] float volumePulo2 = 0.75f;
+    [SerializeField] AudioClip pulo3SFX;
+    [SerializeField] [Range(0, 1)] float volumePulo3 = 0.75f;
+    [SerializeField] AudioClip pulo4SFX;
+    [SerializeField] [Range(0, 1)] float volumePulo4 = 0.75f;
+    [SerializeField] AudioClip puloDuplo1SFX;
+    [SerializeField] [Range(0, 1)] float volumePuloDuplo1 = 0.75f;
+    [SerializeField] AudioClip puloDuplo2SFX;
+    [SerializeField] [Range(0, 1)] float volumePuloDuplo2 = 0.75f;
+    [SerializeField] AudioClip puloDuplo3SFX;
+    [SerializeField] [Range(0, 1)] float volumePuloDuplo3 = 0.75f;
+    [SerializeField] AudioClip puloDuplo4SFX;
+    [SerializeField] [Range(0, 1)] float volumePuloDuplo4 = 0.75f;
+    [SerializeField] AudioClip slideSFX;
+    [SerializeField] [Range(0, 1)] float volumeSlide = 0.75f;
+   
 
     void Start()
     {
@@ -46,11 +69,15 @@ public class PlayerController : MonoBehaviour
                 body.velocity = new Vector2(body.velocity.x, pulo);
                 puloDouble = true; //no tutorial deixar false
                 puloCount = 0;
-                soundManager.PlayAudio("puloduplo");
                 Debug.Log("Pulo Doble funcionou");
                 animator.SetBool("Grounded", false);
                 gameObject.GetComponent<BoxCollider2D>().enabled = true;
                 gameObject.GetComponent<CapsuleCollider2D>().enabled = false;
+                var randomizadorPuloDuplo = Random.Range(0, 3);
+                if (randomizadorPuloDuplo == 0) { PuloDuploSFX1(); }
+                if (randomizadorPuloDuplo == 1) { PuloDuploSFX2(); }
+                if (randomizadorPuloDuplo == 2) { PuloDuploSFX3(); }
+                if (randomizadorPuloDuplo == 3) { PuloDuploSFX4(); }
             }
 
             if (grounded && (puloCount == 0))
@@ -63,6 +90,11 @@ public class PlayerController : MonoBehaviour
                 animator.SetBool("Grounded", false);
                 gameObject.GetComponent<BoxCollider2D>().enabled = true;
                 gameObject.GetComponent<CapsuleCollider2D>().enabled = false;
+                var randomizadorPulo = Random.Range(0, 3);
+                if (randomizadorPulo == 0) { PuloSFX1(); }
+                if (randomizadorPulo == 1) { PuloSFX2(); }
+                if (randomizadorPulo == 2) { PuloSFX3(); }
+                if (randomizadorPulo == 3) { PuloSFX4(); }
                 //soundManager.PlayAudio("pulo");
             }
             //if (podePular == true)
@@ -93,6 +125,7 @@ public class PlayerController : MonoBehaviour
             if (grounded && podeSlide == true)
             {
                 animator.SetTrigger("Deslizando");
+                SlideSFX();
                 //gameObject.GetComponent<BoxCollider2D>().size = new Vector2 (0.01f,0.01f);
                 GetComponentInChildren<BoxCollider2D>().enabled = false;
                 //gameObject.GetComponent<BoxCollider2D>().enabled = false;
@@ -143,10 +176,11 @@ public class PlayerController : MonoBehaviour
         }
         switch (collision.gameObject.tag)
         {
+            
             case "Dead":
             case "Obstaculo":
-                soundManager.PlayAudio("obstaculocolision");
-                
+
+                soundManager.PlayAudio("obstaculocolision");                
                 gameManager.RestartGame();
                 break;
         }         
@@ -197,9 +231,11 @@ public class PlayerController : MonoBehaviour
             Destroy(other.gameObject);
             puloDouble = true;
             //soundManager.PlayAudio("puloduplo");
-
         }
-        
+        if (other.tag == "SomMorreuTut")
+        {
+            AudioSource.PlayClipAtPoint(deathSFX, Camera.main.transform.position, volumeMorte);
+        }
     }
     
     //Parar e seguir no tutorial
@@ -221,5 +257,49 @@ public class PlayerController : MonoBehaviour
     {
         contatempoTUT += Time.deltaTime;
     }
+    private void PuloDuploSFX1()
+    {
+        Debug.Log("puloDuplo 1");
+        AudioSource.PlayClipAtPoint(puloDuplo1SFX, Camera.main.transform.position, volumePuloDuplo1);
+    }
+    private void PuloDuploSFX2()
+    {
+        Debug.Log("puloDuplo 2");
+        AudioSource.PlayClipAtPoint(puloDuplo2SFX, Camera.main.transform.position, volumePuloDuplo2);
+    }
+    private void PuloDuploSFX3()
+    {
+        Debug.Log("puloDuplo 3");
+        AudioSource.PlayClipAtPoint(puloDuplo3SFX, Camera.main.transform.position, volumePuloDuplo3);
+    }
+    private void PuloDuploSFX4()
+    {
+        Debug.Log("puloDuplo 4");
+        AudioSource.PlayClipAtPoint(puloDuplo4SFX, Camera.main.transform.position, volumePuloDuplo4);
+    }
+    private void PuloSFX1()
+    {
+        Debug.Log("pulo 1");
+        AudioSource.PlayClipAtPoint(pulo1SFX, Camera.main.transform.position, volumePulo1);
+    }
+    private void PuloSFX2()
+    {
+        Debug.Log("pulo 2");
+        AudioSource.PlayClipAtPoint(pulo2SFX, Camera.main.transform.position, volumePulo2);
+    }
+    private void PuloSFX3()
+    {
+        Debug.Log("pulo 3");
+        AudioSource.PlayClipAtPoint(pulo3SFX, Camera.main.transform.position, volumePulo3);
+    }
+    private void PuloSFX4()
+    {
+        Debug.Log("pulo 4");
+        AudioSource.PlayClipAtPoint(pulo4SFX, Camera.main.transform.position, volumePulo4);
+    }
+    private void SlideSFX()
+    {
+        AudioSource.PlayClipAtPoint(slideSFX, Camera.main.transform.position, volumeSlide);
+    }  
 
 }
