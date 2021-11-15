d167d2c684398edbb0d5ca5c5c00e0d9eed4a68a
Bruno Café <brunocafezeiro7@gmail.com>
2021-11-15 03:54:25 -0300

    Moeda Tutorial 80%
    Falta instanciar no código da personagem e mencionar o desligamento nas animações

---
+++ PB COM CUIDADO/Assets/Scripts/Tutorial/MoetaTuto.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoetaTuto : MonoBehaviour
+{
+    private GameManager gameManager;
+
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "playerCollider")
+        {
+            Destroy(gameObject);
+            gameManager.AddPontos(10);
+        }
+
+    }
+}
