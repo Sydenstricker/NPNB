014e6de5ce60b9af331cbfecc046f46921a62bc9
Jéssica <jeunsantos@gmail.com>
2021-12-14 21:20:45 -0300

    Sem movimentos durante o Pause

--- PB COM CUIDADO/Assets/Scripts/PauseMenu.cs
+++ PB COM CUIDADO/Assets/Scripts/PauseMenu.cs
@@ -4,11 +4,14 @@ public class PauseMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     private bool naveTutorialMorreu = false;
-
     public static bool GameisPaused = true;
-
     public GameObject pauseMenuUI;
+
+    [SerializeField] bool isTutorial = false;
+    [SerializeField] bool isCave = false;
+    [SerializeField] bool isNoShoot = false;
     [SerializeField] bool isNave = false;
+
     private bool playerMorreu = false;
 
     private void Start()
@@ -37,15 +40,27 @@ public class PauseMenu : MonoBehaviour
         Cursor.visible = false;
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
-        GameisPaused = false;
-        //GameObject.FindWithTag("Player").GetComponent<PlayerController>().enabled = true;
-        //GameObject.FindWithTag("Player").GetComponent<PlayerCav>().enabled = true;
-        //GameObject.FindWithTag("Player").GetComponent<Player>().enabled = true;
+        GameisPaused = false;        
 
+       //Permitir movimentos de Pinky após o Pause em cada fase
+        if (isTutorial)
+        {
+            GameObject.FindWithTag("Player").GetComponent<PlayerController>().enabled = true;
+        }
+
+        if (isCave)
+        {
+            GameObject.FindWithTag("Player").GetComponent<PlayerCav>().enabled = true;
+        }
+
+        if (isNoShoot)
+        {
+            GameObject.FindWithTag("Player").GetComponent<Player>().enabled = true;
+        }
 
         if (isNave)
-        {
-           FindObjectOfType<StationTutorialSaiDeCena>().NaoTiraPlataformaCenaTutorial();           
+        {            
+            FindObjectOfType<StationTutorialSaiDeCena>().NaoTiraPlataformaCenaTutorial();           
 
             if (naveTutorialMorreu)
             {
@@ -62,13 +77,25 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 0f;
         GameisPaused = true;        
         Cursor.visible = true;
-        //GameObject.FindWithTag("Player").GetComponent<PlayerController>().enabled = false;
-        //GameObject.FindWithTag("Player").GetComponent<PlayerCav>().enabled = false;
-        //GameObject.FindWithTag("Player").GetComponent<Player>().enabled = false;
 
+        //Parar movimentos de Pinky durante o Pause em cada fase
+        if (isTutorial)
+        {
+            GameObject.FindWithTag("Player").GetComponent<PlayerController>().enabled = false;
+        }
+
+        if (isCave)
+        {
+            GameObject.FindWithTag("Player").GetComponent<PlayerCav>().enabled = false;
+        }
+
+        if (isNoShoot)
+        {
+            GameObject.FindWithTag("Player").GetComponent<Player>().enabled = false;
+        }
 
         if (isNave)
-        {
+        {            
             FindObjectOfType<StationTutorialSaiDeCena>().NaoTiraPlataformaCenaTutorial();            
 
             if (naveTutorialMorreu) { FindObjectOfType<StationTutorialSaiDeCena>().NaoTiraPlataformaCenaTutorial(); }
